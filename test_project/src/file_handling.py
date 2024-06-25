ded0181c63e3e85fba4a26c206a6aa50bf585408
Jessicavazm <jjessicavaz@gmail.com>
2024-06-25 18:48:31 +1000

    File handlingexamples

---
+++ test_project/src/file_handling.py
@@ -0,0 +1,39 @@
+# Open a file in write mode, if file doesn't exist, it creates one. Always remember to store file in variable. "File" in this case == variable.
+# \n breaks the line, it goes into the next line == <br>
+file = open('newFile.txt', 'w')
+file.write("This is a new File!!!\n")
+file.close()
+
+# Open a file in read mode
+file = open('newFile.txt', 'r')
+content = file.read()
+
+print(content)
+file.close()
+
+
+# Open the file in append mode
+file = open('newFile.txt', 'a')
+file.write("This is my second line!!!\n")
+file.close()
+
+file = open('newFile.txt', 'r')
+content = file.read()
+print(content)
+file.close()
+
+# with statement
+with open ('newFile.txt', 'a') as file:
+    file.write("Third line reporting.")
+
+with open ('newFile.txt', 'r') as file:
+    content = file.read()
+    print(content)
+
+with open ('newFile,txt', 'r') as file:
+    content = file.readline()
+
+
+
+# with open ('newFile.txt', 'r') as file:
+#     content = file.readline()
